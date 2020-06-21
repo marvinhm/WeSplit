04e10225137ef4460984a256f4da5aeeaef847ca
marvinhm <marvinmatovu@gmail.com>
2020-06-21 12:36:56 +0100

    Adding a navigation bar

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,15 +8,20 @@
 
 import SwiftUI
     
-  struct ContentView: View {
-       @State var count = 0
-
-       var body: some View {
-           Button("Tap Count: \(count)") {
-               self.count += 1
-           }
-       }
-   }
+struct ContentView: View {
+    @State var count = 0
+   
+    var body: some View {
+        NavigationView {
+            Section {
+                Button("Tap Count: \(count)") {
+                   self.count += 1
+                }
+            }
+            .navigationBarTitle("Duka X Swift", displayMode: .inline)
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
